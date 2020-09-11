3ff5263237b573da49fd7cd70d039e3683c2a122
Farhana Yasmin <farhana@zool.in>
2020-09-11 21:59:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send({hi:'Farhana'})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(5000);
