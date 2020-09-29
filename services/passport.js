b5b9f3ad2c987633993b6c8fe165180897db97d1
Farhana Yasmin <farhana@zool.in>
2020-09-29 16:54:54 +0530

    changed to add proxy

--- services/passport.js
+++ services/passport.js
@@ -16,7 +16,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: "http://localhost:5000/auth/google/callback",
+        callbackURL: "/auth/google/callback",
         proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
